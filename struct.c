48989746b8069b994fb9d9cc6d69b1071a4a93f3
nayanamohan2000 <48672725+nayanamohan2000@users.noreply.github.com>
2019-05-24 09:08:08 +0530

    Create struct.c

---
+++ struct.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void get_input(int *a,int *b,iny *c)
+{
+    printf("enter the values of a,b,c\n");
+    scanf("%d %d %d",a,b,c);
+}
+void copare(int a,int b,int c)
+{
+    if(a>b)
+    {
+        if(a>c)
+         {
+          return a;
+
+
+
